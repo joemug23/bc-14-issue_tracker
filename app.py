5674196e4e3deb1659023f2446abd48568cef2af
Jonathan Mugula Zihalirwa <Jonathan Mugula Zihalirwa>
2017-01-24 00:00:33 +0300

    Trying social Auth login

--- app.py
+++ app.py
@@ -1,21 +1,78 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect, session, flash
 from database import Database
+from flask_oauth import OAuth
+
+SECRET_KEY = 'development key'
+
 
 app = Flask(__name__)
+app.secret_key = SECRET_KEY
+oauth = OAuth()
 
+# Use Twitter as example remote application
+twitter = oauth.remote_app('twitter',
+    # unless absolute urls are used to make requests, this will be added
+    # before all URLs.  This is also true for request_token_url and others.
+    base_url='https://api.twitter.com/1/',
+    # where flask should look for new request tokens
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    # where flask should exchange the token with the remote application
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    # twitter knows two authorizatiom URLs.  /authorize and /authenticate.
+    # they mostly work the same, but for sign on /authenticate is
+    # expected because this will give the user a slightly different
+    # user interface on the twitter side.
+    authorize_url='https://api.twitter.com/oauth/authenticate',
+    # the consumer keys from the twitter application registry.
+    consumer_key='uiXanIM81M3RiDjRzWyvREyof',
+    consumer_secret='XkW5ihmwsD88pN3j0eyMSEdtLoDyIRi9ipIZb4HyS4QpA0Zj5L'
+)
 
 @app.route('/')
 def index():
-	return render_template('main.html')
+    return render_template('main.html')
+
 
 @app.route('/add')
 def got_to_add():
-	return render_template('add_issue.html')
+    return render_template('add_issue.html')
 
-@app.route('login')
+
+@app.route('/login')
 def login():
-	return render_template('login.html')
+    return render_template('login.html')
 
-	
+@app.route('/twitter_login')
+def twitter_login():
+    return twitter.authorize(callback=url_for('oauth_authorized',
+                                              next=request.args.get('next') or request.referrer or None))
+
+@app.route('/logout')
+def logout():
+    session.pop('screen_name', None)
+    flash('You were signed out')
+    return redirect(request.referrer or url_for('index'))
+
+
+@app.route('/oauth-authorized')
+@twitter.authorized_handler
+def oauth_authorized(resp):
+    next_url = request.args.get('next') or url_for('index')
+    if resp is None:
+        flash(u'You denied the request to sign in.')
+        return redirect(next_url)
+
+    access_token = resp['oauth_token']
+    session['access_token'] = access_token
+    session['screen_name'] = resp['screen_name']
+
+    session['twitter_token'] = (
+        resp['oauth_token'],
+        resp['oauth_token_secret']
+    )
+
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
-	app.run()
+    app.run()
--- database.py
+++ database.py
@@ -36,10 +36,10 @@ class Database:
         # construct and call an update query
         try:
             slist = sorted(params.keys())
-            db_id = params[table+"_id"]
+            db_id = params[table + "_id"]
             query = 'update {} set {} where {} = {}'.format(table,
-                                                             ','.join([v+"='{}'".format(params[v]) for v in slist]),
-                                                                 table+"_id", db_id)
+                                                            ','.join([v + "='{}'".format(params[v]) for v in slist]),
+                                                            table + "_id", db_id)
             Database.db.execute(query)
             Database.db.commit()
             return "Data updated successfully!"
@@ -50,8 +50,8 @@ class Database:
     def deleting(table, params):
         # Construct and call a delete query
         try:
-            db_id = params[table+"_id"]
-            query = 'delete from {} where {} = {}'.format(table, table+"_id", db_id)
+            db_id = params[table + "_id"]
+            query = 'delete from {} where {} = {}'.format(table, table + "_id", db_id)
 
             print(query)
             Database.db.execute(query)
@@ -68,7 +68,7 @@ class Database:
             cursor = Database.db.execute(query)
             for row in cursor:
                 # adding each row to the results dictionary
-                results[row[table+"_id"]] = dict(row)
+                results[row[table + "_id"]] = dict(row)
             print(results)
             return results
         except Exception as e:
@@ -90,20 +90,42 @@ class Database:
         except Exception as e:
             return "An error occurred: {}".format(e)
 
+
 def main():
     # Database.select_all
     # db.row_factory = sqlite3.Row
+    Database.db.execute("drop table if exists issues")
+    Database.db.execute(
+        'create table issues(issues_id INTEGER PRIMARY KEY, '
+        'description text, '
+        'department text, '
+        'priority text, '
+        'status text DEFAULT "open",'
+        'comment text, '
+        'assigned_to text Default "No one",'
+        'raise_person text)')
+
     Database.db.execute("drop table if exists users")
-    Database.db.execute('create table users(users_id INTEGER PRIMARY KEY, username text, password text)')
-    Database.insert('users', {'username':'Jonathan','password':'123'})
-    Database.insert('users', {'username': 'Mugula', 'password': 'pass'})
-    Database.insert('users', {'username': 'Zihalirwa', 'password': '456'})
-    Database.select_all('users')
+    Database.db.execute(
+        'create table users(users_id INTEGER PRIMARY KEY, '
+        'username text, '
+        'email text, '
+        'status text DEFAULT "staff", '
+        'department text, '
+        'password text)')
+    Database.insert('users', {'username':'Jonathan', 'email':'joemzih23@gmail.com', 'status': 'admin', 'department': 'Operation', 'password':'mugula'})
+    Database.insert('users', {'username': 'joemug', 'email': 'joemzih23@gmail.com', 'status': 'staff',
+                              'department': 'Operation', 'password': 'mugula'})
+
+    # Database.insert('users', {'username': 'Mugula', 'password': 'pass'})
+    # Database.insert('users', {'username': 'Zihalirwa', 'password': '456'})
+    # Database.select_all('users')
     # db.execute('insert into users (username, password) values(?, ?)', ('Joemug', '123'))
     # db.execute('insert into users (username, password) values(?, ?)', ('joe', 'joe'))
-    # db.commit()
+    # Database.db.commit()
     # cursor = Database.db.execute('select * from users')
     # for row in cursor:
     #     print(dict(row))
+
 
 if __name__ == '__main__': main()
